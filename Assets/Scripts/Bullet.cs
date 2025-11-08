0e5e59cbd235dabcfa3dd151e339216b9777e40e
Shreya Patel <justf0rsch00lspsp@gmail.com>
2025-11-07 20:51:22 -0500

    update bullet script

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        // transfrom.Translate(Vector3.up * Time.deltaTime * 8f);
         transform.Translate(new Vector3(0, 1, 0) * Time.deltaTime * 8f);
         if (transform.position.y > 6.5f)
         {
