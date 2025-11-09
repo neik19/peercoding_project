634e417e354999b3f64879949333f678d00fbe5f
tots <serr.tatiana04@gmail.com>
2025-11-09 16:47:07 -0500

    personal scene fixes

---
+++ Assets/Scripts/TatiGameManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using TMPro;
+
+public class TatiGaneManager : MonoBehaviour
+{
+    public GameObject enemyOnePrefab;
+    public GameObject enemyNeilPrefab;
+    public GameObject shreyaEnemyPrefab;
+    public TextMeshProUGUI livesText;
+    public int score;
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+        score = 0;
+        InvokeRepeating("CreateEnemyOne", 2.5f, 3f);
+        Invoke("CreateEnemyNeil", 7f);
+        InvokeRepeating("CreateShreyaEnemy", 2f, 5f);
+    }
+
+    void CreateEnemyOne()
+    { 
+        Debug.Log("Enemy One created");
+
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8f, 8f), 6.5f, 0), Quaternion.identity);
+    }
+
+        void CreateEnemyNeil()
+    { 
+        Debug.Log("Enemy Neil created");
+
+        Instantiate(enemyNeilPrefab, new Vector3(Random.Range(-8f, 8f), 4.5f, 0), Quaternion.identity);
+    }
+
+    void CreateShreyaEnemy()
+    {
+        Instantiate(shreyaEnemyPrefab, new Vector3(Random.Range(-8f, 8f), 4.5f, 0), Quaternion.identity);
+    }
+}    
